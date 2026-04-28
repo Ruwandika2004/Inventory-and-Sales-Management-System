727f943a729848e0a8b58b0b4cadedce6830d862
Ruwandika2004 <it25102341@my.sliit.lk>
2026-04-29 00:19:53 +0530

    Day 1: Initial project setup and User entity structure

---
+++ src/main/java/com/supermarket/inventory/entity/User.java
@@ -0,0 +1,16 @@
+package com.supermarket.inventory.entity;
+
+public abstract class User {
+    private String username;
+    private String password;
+    private String role; // "ADMIN" or "STAFF"
+
+    public User() {
+    }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
