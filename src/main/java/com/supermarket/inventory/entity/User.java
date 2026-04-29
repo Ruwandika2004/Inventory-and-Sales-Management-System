3cd9bcea292bed1ffbb21c8be1a00f9d543832e8
Ruwandika2004 <it25102341@my.sliit.lk>
2026-04-29 08:57:35 +0530

    Added getters/setters and created Admin/Customer subclasses

--- src/main/java/com/supermarket/inventory/entity/User.java
+++ src/main/java/com/supermarket/inventory/entity/User.java
@@ -13,4 +13,16 @@ public abstract class User {
         this.password = password;
         this.role = role;
     }
+    public String getUsername()              { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword()              { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole()                  { return role; }
+    public void setRole(String role)         { this.role = role; }
+
+
+    public abstract String getDashboardRoute();
 }
+
