ba458f89a003e12413dbb46b606528e85efc5f39
it25102000@my.sliit.lk <it25102000@my.sliit.lk>
2026-05-05 21:51:09 +0530

    member_6-day2

---
+++ src/main/java/com/supermarket/inventory/entity/StockReport.java
@@ -0,0 +1,20 @@
+package com.supermarket.inventory.entity;
+
+public class StockReport extends Report {
+    private int totalItemsInStock;
+
+    public StockReport() {}
+
+    public StockReport(String reportId, int totalItemsInStock) {
+        super(reportId);
+        this.totalItemsInStock = totalItemsInStock;
+    }
+
+    public int getTotalItemsInStock() { return totalItemsInStock; }
+    public void setTotalItemsInStock(int totalItemsInStock) { this.totalItemsInStock = totalItemsInStock; }
+
+    @Override
+    public String generateReportContent() {
+        return "Stock Report [ID: " + getReportId() + "] - Total Items in Stock: " + totalItemsInStock;
+    }
+}
